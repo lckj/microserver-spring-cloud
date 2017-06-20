6d783788355e916444ccc1180e887e57f0c3cf04
wcjwctwy <708808508@qq.com>
2017-06-21 00:30:51 +0800

    自定义ribbon

--- microserver-consumer-movie-ribbon/src/main/java/wang/congjun/microserver/ConsumerMovieRibbonApplication.java
+++ microserver-consumer-movie-ribbon/src/main/java/wang/congjun/microserver/ConsumerMovieRibbonApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import wang.congjun.config.RibbonConfugration;
 
 /**
  * 用MyBatis作为数据处理层
@@ -13,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+//自定义ribbonclient的配置 配置的信息在RibbonConfugration这个类中（这个类不能在Spring扫描的范围内）
+@RibbonClient(name = "microserver-producer-user" , configuration = RibbonConfugration.class)
 public class ConsumerMovieRibbonApplication {
 
 	@Bean
@@ -20,6 +24,8 @@ public class ConsumerMovieRibbonApplication {
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerMovieRibbonApplication.class, args);
